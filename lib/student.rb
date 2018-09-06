a19bbcfc179e5b4bf7fadcc7216e644e22044429
Jonathan Chan <johnkchan252@gmail.com>
2018-09-06 02:21:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      Student.new(student)
-    end
+    students_array.each{|student_info| Student.new(student_info)}
   end
 
   def add_student_attributes(attributes_hash)
